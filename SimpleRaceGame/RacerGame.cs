37d5aa14abb03f6a47730765801e9b82ac4cff26
Ilya Petrov <ipb.tech@yandex.ru>
2023-06-06 00:16:09 +0300

    Pre-design

---
+++ SimpleRaceGame/RacerGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleRaceGame
+{
+    public partial class RacerGame : Form
+    {
+        public RacerGame()
+        {
+            InitializeComponent();
+        }
+
+        private void RacerGame_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                MessageBoxButtons messageBoxButtons = MessageBoxButtons.YesNo;
+                DialogResult dialog = MessageBox.Show("Вы действительно хотите выйти?", "Выход", messageBoxButtons);
+                if (dialog == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
