86da868eb0603fe42907b1ba816331841aacc114
Ilya Petrov <ipb.tech@yandex.ru>
2023-06-06 23:39:56 +0300

    Moving Background

--- SimpleRaceGame/RacerGame.Designer.cs
+++ SimpleRaceGame/RacerGame.Designer.cs
@@ -28,33 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RacerGame));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RoadPictureMain = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.RoadPictureBack = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.RoadPictureMain)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadPictureBack)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // RoadPictureMain
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(840, 650);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.RoadPictureMain.Image = ((System.Drawing.Image)(resources.GetObject("RoadPictureMain.Image")));
+            this.RoadPictureMain.Location = new System.Drawing.Point(0, 0);
+            this.RoadPictureMain.Name = "RoadPictureMain";
+            this.RoadPictureMain.Size = new System.Drawing.Size(840, 650);
+            this.RoadPictureMain.TabIndex = 0;
+            this.RoadPictureMain.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(421, 500);
+            this.pictureBox2.Location = new System.Drawing.Point(423, 494);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.Size = new System.Drawing.Size(120, 128);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            // 
+            // RoadPictureBack
+            // 
+            this.RoadPictureBack.Image = ((System.Drawing.Image)(resources.GetObject("RoadPictureBack.Image")));
+            this.RoadPictureBack.Location = new System.Drawing.Point(0, -630);
+            this.RoadPictureBack.Name = "RoadPictureBack";
+            this.RoadPictureBack.Size = new System.Drawing.Size(840, 650);
+            this.RoadPictureBack.TabIndex = 2;
+            this.RoadPictureBack.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // RacerGame
             // 
@@ -64,22 +82,27 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(840, 650);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RoadPictureMain);
+            this.Controls.Add(this.RoadPictureBack);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "RacerGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SimpleRacerGame";
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RacerGame_KeyPress);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadPictureMain)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadPictureBack)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox RoadPictureMain;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox RoadPictureBack;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- SimpleRaceGame/RacerGame.cs
+++ SimpleRaceGame/RacerGame.cs
@@ -15,18 +15,42 @@ namespace SimpleRaceGame
         public RacerGame()
         {
             InitializeComponent();
+            Timer timer = new Timer();
+            timer.Tick += timer_Tick;
+
         }
 
         private void RacerGame_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Escape)
             {
+                timer.Stop();
                 MessageBoxButtons messageBoxButtons = MessageBoxButtons.YesNo;
-                DialogResult dialog = MessageBox.Show("Вы действительно хотите выйти?", "Выход", messageBoxButtons);
+                DialogResult dialog = MessageBox.Show("Are you sure want to exit?", "Exit", messageBoxButtons);
                 if (dialog == DialogResult.Yes)
                 {
                     this.Close();
                 }
+                else
+                {
+                    timer.Start();
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            int speed = 10;
+            RoadPictureMain.Top += speed;
+            RoadPictureBack.Top += speed;
+            //RoadPictureBack.Top += (int)(speed * 1.1);
+
+            if (RoadPictureMain.Top == this.Height)
+            {
+                RoadPictureMain.Top = 0;
+                RoadPictureBack.Top = -this.Height;
+                //RoadPictureBack.Top = (int)(-this.Height * 0.95);
             }
         }
     }
